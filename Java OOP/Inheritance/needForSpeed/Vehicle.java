0a4a8455b21b3213580b7381f2b9e763ef5c8844
Gpandev <44357591+Gpandev@users.noreply.github.com>
2019-07-29 11:10:26 +0300

    upload

---
+++ Java OOP/Inheritance/needForSpeed/Vehicle.java
@@ -0,0 +1,49 @@
+package needForSpeed;
+
+public class Vehicle {
+    private final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = getDefaultFuelConsumption();
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public double getFuelConsumption() {
+        return this.fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return this.fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void drive(double kilometers) {
+        double fuelLeft = this.fuelConsumption * kilometers;
+        if (fuelLeft <= this.fuel) {
+            this.fuel -= fuelLeft;
+        }
+    }
+}
