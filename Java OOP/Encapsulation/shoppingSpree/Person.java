906287eed28a923370e7795bba122f4c8040e702
Gpandev <44357591+Gpandev@users.noreply.github.com>
2019-08-01 13:53:22 +0300

    Upload

---
+++ Java OOP/Encapsulation/shoppingSpree/Person.java
@@ -0,0 +1,51 @@
+package shoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        this.setName(name);
+        this.setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw  new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setMoney(double money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+
+        this.money = money;
+    }
+
+    public void buyProduct(Product product) {
+        if (this.money < product.getCost()) {
+          throw new IllegalArgumentException(String.format("%s can't afford %s",
+                    this.getName(),
+                    product.getName()));
+
+        }
+
+            this.money -= product.getCost();
+            this.products.add(product);
+
+
+
+    }
+
+}
