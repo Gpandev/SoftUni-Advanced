6f96afb636ff53cbcf1357750da6fb8d9b7ab08e
Gpandev <44357591+Gpandev@users.noreply.github.com>
2019-08-02 18:14:34 +0300

    upload

---
+++ Java OOP/Interfaces And Abstractions/foodShortage/Citizen.java
@@ -0,0 +1,50 @@
+package foodShortage;
+
+import foodShortage.interfaces.Buyer;
+import foodShortage.interfaces.Identifiable;
+import foodShortage.interfaces.Person;
+
+public class Citizen implements Buyer, Identifiable, Person {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+    private int food;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+        this.food = 0;
+    }
+
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public void buyFood() {
+        this.food += 10;
+    }
+
+    @Override
+    public int getFood() {
+        return this.food;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
---
+++ Java OOP/Interfaces And Abstractions/foodShortage/interfaces/Person.java
@@ -0,0 +1,6 @@
+package foodShortage.interfaces;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
